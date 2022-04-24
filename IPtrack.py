06e57d23a53be012b9737186d999588eca3d23d1
Hash$ <89229676+HashCipher@users.noreply.github.com>
2022-04-24 18:33:21 -0400

    Update IPtrack.py

--- IPtrack.py
+++ IPtrack.py
@@ -4,18 +4,19 @@ import time
 import os
 import json
 from urllib import request
-
-print("--- IP Tracker ---")
-print("")
-time.sleep(2)
-os.system("clear")
-
+os.system('cls' if os.name == 'nt' else 'clear')
 url = "http://ip-api.com/json/"
-ip = input("IP: ")
-request = request.urlopen(url + ip)
-data_json = json.loads(request.read())
+print("--- Simple IP Tracker ---")
 
-if data_json['status'] == "success":
+def request_and_input():
+    print('<enter> for your own ip, or 0 to leave.')
+    ip = input(": ")
+    if ip == 0:
+	quit()
+    request = request.urlopen(url + ip)
+    data_json = json.loads(request.read())
+
+    if data_json['status'] == "success":
 	print(f"IP : {data_json['query']}")
 	print(f"País : {data_json['country']}")
 	print(f"Região : {data_json['regionName']}")
@@ -23,5 +24,8 @@ if data_json['status'] == "success":
 	print(f"Latitude : {data_json['lat']}")
 	print(f"Longitude : {data_json['lon']}")
 	print(f"ISP : {data_json['isp']}")
-else:
-	print("IP inválido!")
+    else:
+	print("Invalid IP!")
+    
+while(True):
+    request_and_input()
